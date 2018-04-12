5ddc1737cad7b8bb84e09a6fcde6732b6b70956c
Alvin Yan <yychuyu@163.com>
2018-04-12 20:29:52 +0800

    finish simple

---
+++ simple/fun1.h
@@ -0,0 +1,7 @@
+#ifndef FUN1_H_
+#define FUN1_H_
+
+void fun1();
+
+
+#endif
---
+++ simple/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "fun1.h"
+#include "fun2.h"
+
+int main()
+{
+	printf("hello world\n");
+	fun1();
+	fun2();
+}
